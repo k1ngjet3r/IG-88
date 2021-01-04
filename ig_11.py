f2134616b47e60fec747bd03694b22b5d7c5bc48
Jeter@work <43425262+k1ngjet3r@users.noreply.github.com>
2021-01-04 12:04:38 +0800

    Update ig_11.py

--- ig_11.py
+++ ig_11.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome('/Users/Jeter/Downloads/chromedriver')
+driver = webdriver.Chrome()
 
 messenger = 'https://www.facebook.com/messages/t/100000021312021/'
 driver.get(messenger)
@@ -13,8 +13,7 @@ driver.find_element_by_name('email').send_keys(username)
 driver.find_element_by_name('pass').send_keys(pw)
 driver.find_element_by_name('pass').send_keys(Keys.ENTER)
 
-message_field = driver.find_elements_by_xpath(
-    '/html/body/div[1]/div/div[1]/div[1]/div[3]/div/div/div[1]/div[1]/div[2]/div/div/div/div/div/div[1]/div[2]/div/div/div/div[2]/div/form/div/div[3]/div[2]/div[1]/div/div/div/div/div[2]/div/div/div/div')
+message_field = driver.find_elements_by_class_name('_1mf _1mj')
 
 message_field.send_keys('This is the first message sent by the bot!')
 message_field.send_keys(Keys.ENTER)
